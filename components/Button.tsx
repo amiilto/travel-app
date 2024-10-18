d0932c69ead99aa4e469574c97b0595897b59056
Amilto Romagno <amiltoromagno@outlook.com>
2024-10-18 10:01:00 -0300

    feat: add button, camp and features components

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 
-interface ButtonProps {
-  type: "button" | "submit";
+type ButtonProps = {
+  type: 'button' | 'submit';
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 }
 
-export default function Button(props: ButtonProps) {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      type={props.type}
-      className={`flexCenter gap-3 rounded-full border ${props.variant}`}
+    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type}
     >
-      {props.icon && (
-        <Image src={props.icon} alt={props.icon} width={24} height={24} />
-      )}
-      <label className="bold-16 whitespace-nowrap">{props.title}</label>
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
-  );
+  )
 }
+
+export default Button
