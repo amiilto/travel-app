17f048f1d5b484bfadc4b236d9184725c87bc41e
Amilto Romagno <amiltoromagno@outlook.com>
2024-10-19 20:31:27 -0300

    deploy: configure deploy setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/travel-app',
+  reactStrictMode: true,
+  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
